551fb155da7a87846d3495b48319595b1b5f7b6d
niconunezz <niconuwii@gmail.com>
2024-10-15 21:36:52 +0200

    important changes

--- scripts/model.py
+++ scripts/model.py
@@ -7,21 +7,21 @@ from timm.layers.norm_act import BatchNormAct2d
 
 
 class FeatureExtraction(nn.Module):
-    def __init__(self, out_dim ,channels: int = 3, n_landmarks: int = 130):
+    def __init__(self, out_dim ,channels: int = 3, height: int = 130, width: int = 130):
         super(FeatureExtraction, self).__init__()
-        self.in_channels = in_channels = 32 * math.ceil(n_landmarks / 2)
-        self.stem_linear = nn.Linear(in_channels, out_dim, bias=False)
-        self.stem_bn = nn.BatchNorm1d(out_dim, momentum=0.95)
+        self.in_channels = in_channels = 32 * math.ceil(width / 2)
         self.conv_stem = nn.Conv2d(channels, 32, kernel_size=(3, 3), stride=(1,2), padding=(1, 1), bias=False)
         self.bn_conv = BatchNormAct2d(32, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True,act_layer = nn.SiLU,drop_layer=None)
+        self.stem_linear = nn.Linear(height * in_channels, out_dim, bias=False)
+        self.stem_bn = nn.BatchNorm1d(out_dim, momentum=0.95)
 
     def forward(self, data):
-        xc = data.permute(0, 3, 1, 2)
-        xc = self.conv_stem(xc)
-        xc = self.bn_conv(xc)
-        xc = xc.flatten(1)
-        xc = self.stem_linear(xc)
-        xc = self.stem_bn(xc)
+        xc = data.permute(0, 3, 1, 2) # B, C, H, W
+        xc = self.conv_stem(xc) # B, 32, H, W//2
+        xc = self.bn_conv(xc) # B, 32, H, W//2
+        xc = xc.flatten(1) # B, 32 * H * W//2
+        xc = self.stem_linear(xc) # B, out_dim
+        xc = self.stem_bn(xc) # B, out_dim
 
         return xc
 
@@ -30,8 +30,22 @@ class Net(nn.Module):
     def __init__(self, out_dim):
         super(Net, self).__init__()
         self.feature_extraction = FeatureExtraction(out_dim)
+        self.face_fe = FeatureExtraction(out_dim, height=75)
         
 
     def forward(self, data):
-        xc = self.feature_extraction(data)
-        return xc
+        right_hand = data[:, :, :20, :]
+        left_hand = data[:, :, 20:40, :]
+        face = data[:, :, 40:115, :]
+        pose = data[:, :, 115:, :]
+
+
+        all_together = self.feature_extraction(data)
+        face = self.face_fe(face)
+        pose = self.feature_extraction(pose)
+        left_hand = self.feature_extraction(left_hand)
+        right_hand = self.feature_extraction(right_hand)
+
+        xc = torch.cat([face, pose, left_hand, right_hand], dim=1)
+
+        return all_together
