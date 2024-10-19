ce881d92e24ffd9ce5a6574f44cc43d583872f89
niconunezz <niconuwii@gmail.com>
2024-10-20 00:23:47 +0200

    config class

--- scripts/train.py
+++ scripts/train.py
@@ -1,20 +1,28 @@
-from model import Net
+from model.model import Net
 from data import CustomDataset
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from dataclasses import dataclass
 
 
 data = CustomDataset()
 print("Data loaded")
- 
+
+@dataclass
+class config:
+    n_dim: int = 208
+    n_heads: int = 8
+    block_size: int = 16
+
 dataloader = DataLoader(data, batch_size=32, shuffle=False)
+cfg = config()
 
-model = Net(32)
-
+model = Net(cfg)
+    
 for (x, y) in (dataloader):
-    print(x.shape)
+    
     model(x)
     break
