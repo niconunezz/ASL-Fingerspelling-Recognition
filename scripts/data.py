2e4de8904da23b1f116f6e833ce2489e8d58d4ae
niconunezz <niconuwii@gmail.com>
2024-10-24 22:35:33 +0200

    changes for val data

--- scripts/data.py
+++ scripts/data.py
@@ -5,23 +5,31 @@ import numpy as np
 import torch
 
 class CustomDataset(Dataset):
-    def __init__(self) -> None:
-        #! must take out the [:5] for the final version
-        self.files = os.listdir("data/extracted")[1:5]
+    def __init__(self, validation = False) -> None:
+        path = "data/extracted"
+        self.files = os.listdir(path)
+        if validation:
+            path = "data/validation"
+            self.files = os.listdir(path)
+        print(f"Loading {len(self.files)} files")
         self.data = []
         self.labels = []
 
+        counter = 0
         for file in self.files:
-            f = np.load(f"data/extracted/{file}")
+            f = np.load(f"{path}/{file}")
             x = torch.from_numpy(f['arr_0'])
             x = self.fill_nans(x)
             
             try:
                 y = torch.tensor(f['arr_1'])
             except TypeError:
+                counter += 1
                 continue
             self.data.extend(torch.unbind(x, dim=0))
             self.labels.extend(torch.unbind(y,dim=0))
+        
+        print(f"Skipped {counter} files")
 
     
     def fill_nans(self,x):
