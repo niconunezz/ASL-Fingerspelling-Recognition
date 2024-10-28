429deb0d22c5604f6a1919e944a1e5eeb9dfe7b6
niconunezz <niconuwii@gmail.com>
2024-10-28 23:37:36 +0100

    kinda cleaned

--- scripts/model/decoder.py
+++ scripts/model/decoder.py
@@ -118,9 +118,7 @@ class Decoder(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        # each token directly reads off the logits for the next token from a lookup table
-        # self.token_embedding_table = nn.Embedding(config.vocab_size, config.n_dim)
-        # self.position_embedding_table = nn.Embedding(block_size, config.n_dim)
+        
 
         self.layers = nn.ModuleList()
         for _ in range(config.n_layer):
@@ -144,10 +142,6 @@ class Decoder(nn.Module):
 
         self.freqs_cis = self.freqs_cis.to(x.device)
 
-        # idx and targets are both (B,T) tensor of integers
-        # tok_emb = self.token_embedding_table(idx) # (B,T,C)
-        # pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
-        # x = tok_emb # (B,T,C)
         for layer in self.layers:
             x = layer(x, self.freqs_cis)
         
@@ -161,25 +155,9 @@ class Decoder(nn.Module):
             B, T, C = logits.shape
             
             targets = targets.view(B*T)
-            print(f"Targets: {targets.min()}, {targets.max()}")
-            print(f"Logits: {logits.shape}")
-            loss = F.cross_entropy(logits.view(B*T, C), targets, ignore_index=501)
+           
+            loss = F.cross_entropy(logits.view(B*T, C), targets, ignore_index=502)
 
         return logits, loss
 
-    # def generate(self, idx, max_new_tokens):
-    #     # idx is (B, T) array of indices in the current context
-    #     for _ in range(max_new_tokens):
-    #         # crop idx to the last block_size tokens
-    #         idx_cond = idx[:, -block_size:]
-    #         # get the predictions
-    #         logits, loss = self(idx_cond)
-    #         # focus only on the last time step
-    #         logits = logits[:, -1, :] # becomes (B, C)
-    #         # apply softmax to get probabilities
-    #         probs = F.softmax(logits, dim=-1) # (B, C)
-    #         # sample from the distribution
-    #         idx_next = torch.multinomial(probs, num_samples=1) # (B, 1)
-    #         # append sampled index to the running sequence
-    #         idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
-    #     return idx
+    
