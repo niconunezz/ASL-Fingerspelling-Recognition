70f39517ea5f6400f0c813ad991884e8380fd375
niconunezz <niconuwii@gmail.com>
2024-11-14 23:50:40 +0100

    done, time to optimize :P

--- scripts/new_data_extraction.py
+++ scripts/new_data_extraction.py
@@ -1,28 +1,12 @@
 import cv2
 import mediapipe as mp
 import pandas as pd
+import os
+import time
 
 
-"""
-    data structure:
-     {
-        - row = frame,
-        - columns :
-            - right_hand_landmarks_1_x
-            - ...
-            - right_hand_landmarks_1_z
-            - ...
-            - right_hand_landmarks_21_x
-            - ...
-            - right_hand_landmarks_21_z
-            - left_hand_landmarks_1_x
-            - ...
-            - left_hand_landmarks_21_z
-     }
-"""
 
-def detect_sign_language(frame, data):
-    # Initialize MediaPipe Hands
+def detect_sign_language(frame, data, path):
     
     mp_hands = mp.solutions.hands
    
@@ -31,82 +15,101 @@ def detect_sign_language(frame, data):
         max_num_hands=2,
         min_detection_confidence=0.5,
         ) as hands:
-        # Convert the BGR image to RGB
         
-    # Process the frame
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(frame)
 
     
-    if results.multi_hand_landmarks: # If hands are detected
+    if results.multi_hand_landmarks: 
         assert len(results.multi_hand_landmarks) <= 2, "More than 2 hands detected"
 
         if len(results.multi_hand_landmarks) == 2:
             hand_types = ["right", "left"]
         else:
+            
             hand_types = ["right"]
-            
-
-        for landmarks, hand_type in zip(results.multi_hand_landmarks, hand_types): # For each hand
-            
-
-            assert len(landmarks.landmark) == 21, "Not all landmarks detected"
-            
-            for point, n in zip(landmarks.landmark, [i for i in range(21)]):
-                if len(hand_types) == 1:
-                    
-                    data[f"left_hand_{n}_x"].append("nan")
-                    data[f"left_hand_{n}_y"].append("nan")
-                    data[f"left_hand_{n}_z"].append("nan")
-
-                    data[f"{hand_type}_hand_{n}_x"].append(point.x)
-                    data[f"{hand_type}_hand_{n}_y"].append(point.y)
-                    data[f"{hand_type}_hand_{n}_z"].append(point.z)
-                        
-                else:
-                    data[f"{hand_type}_hand_{n}_x"].append(point.x)
-                    data[f"{hand_type}_hand_{n}_y"].append(point.y)
-                    data[f"{hand_type}_hand_{n}_z"].append(point.z)
-
-                    
-             
         
+
+        data["video_id"].append(path)
+      
+        mhand_landmarks = results.multi_hand_landmarks
+        if len(results.multi_hand_landmarks) == 1:
+            for point, n in zip(mhand_landmarks[0].landmark, [i for i in range(21)]):
+                data[f"left_hand_{n}_x"].append(None)
+                data[f"left_hand_{n}_y"].append(None)
+                data[f"left_hand_{n}_z"].append(None)
+
+                data[f"right_hand_{n}_x"].append(point.x)
+                data[f"right_hand_{n}_y"].append(point.y)
+                data[f"right_hand_{n}_z"].append(point.z)
+        else:
+
+            for landmarks, hand_type in zip(mhand_landmarks, hand_types): # For each hand
+            
+                assert len(landmarks.landmark) == 21, "Not all landmarks detected"
+                
+                for point, n in zip(landmarks.landmark, [i for i in range(21)]):
+
+                    data[f"{hand_type}_hand_{n}_x"].append(point.x)
+                    data[f"{hand_type}_hand_{n}_y"].append(point.y)
+                    data[f"{hand_type}_hand_{n}_z"].append(point.z)
+
+            
+
     return data
 
-def main():
+def extract_video(path, data):
+    cap = cv2.VideoCapture(path)
 
-    cols = [f"{hand}_{i}_{dim}" for hand in ["right_hand", "left_hand"] for i in range(21) for dim in ['x', 'y', 'z']]
-    data = {col: [] for col in cols}
-    
-    # Read a video
-    video_path = 'data/videos/69532.mp4'
-    cap = cv2.VideoCapture(video_path)
-    
     if not cap.isOpened():
         print("Error: Could not open video file")
         return 
         
-    try:
-        while True:
-            ret, frame = cap.read()
-            
-            if not ret:
-                print("End of video stream")
-                break
+    while True:
+        ret, frame = cap.read()
         
-            data = detect_sign_language(frame, data)
-            # print(data)
-            
-            
-                
-    finally:
+        if not ret:
+            print("End of video stream")
+            return data
         
+        data = detect_sign_language(frame, data, path)
+
+
+
+def extract_file(data, videos, index):
+    for path in videos:
+        data = extract_video(f"data/videos/{path}", data)
+
+        
+        t0 = time.time()
         df = pd.DataFrame(data)
+        # TODO: must make video_id index
+        df.to_parquet(f"data/files/{index}.parquet", index=False)
+        
 
-        df.head(5)
-        df.to_csv("data/69532.csv", index=False)
-        cap.release()
-        cv2.destroyAllWindows()
+        t1 = time.time()
+        print(f"Time taken: {t1-t0}")
 
+
+
+
+def main():
+
+    cols = [f"{hand}_{i}_{dim}" for hand in ["right_hand", "left_hand"] for i in range(21) for dim in ['x', 'y', 'z']]
+    cols.append("video_id")
+    data = {col: [] for col in cols}
+    
+    
+    vid_per_file = 2
+
+    videos = os.listdir("data/videos")
+    indexes = [i for i in range(0, len(videos),vid_per_file)]
+
+
+    for index, interval in enumerate(indexes):
+        extract_file(data, videos[interval:interval+vid_per_file], index)
+
+    
+    
 if __name__ == "__main__":
     main()
