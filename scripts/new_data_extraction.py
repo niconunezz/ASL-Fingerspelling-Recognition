d5c2442b636a76ff21bddec706aacad5e91a88fd
niconunezz <niconuwii@gmail.com>
2024-11-14 21:43:32 +0100

    one file extraction finally

--- scripts/new_data_extraction.py
+++ scripts/new_data_extraction.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+import pandas as pd
+
 
 """
     data structure:
@@ -19,52 +21,63 @@ import mediapipe as mp
      }
 """
 
-def detect_sign_language(frame):
+def detect_sign_language(frame, data):
     # Initialize MediaPipe Hands
-    import os
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    mp_hands = mp.solutions.hands
     
-    hands = mp_hands.Hands(
-        static_image_mode=False,
-        max_num_hands=2,
-        min_detection_confidence=0.65
-    )
+    mp_hands = mp.solutions.hands
+   
     with mp_hands.Hands(
         static_image_mode=False,
         max_num_hands=2,
-        min_detection_confidence=0.65,
+        min_detection_confidence=0.5,
         ) as hands:
         # Convert the BGR image to RGB
         
     # Process the frame
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = hands.process(frame)
-        print("kfhsbdfihdsjk")
-
-
 
     
-    hands_landmarks = []
     if results.multi_hand_landmarks: # If hands are detected
+        assert len(results.multi_hand_landmarks) <= 2, "More than 2 hands detected"
 
-        for landmarks in results.multi_hand_landmarks: # For each hand
-            kpoints = []
-            for point in landmarks.landmark:
-                kpoints.append({
-                    'x': point.x,
-                    'y': point.y,
-                    'z': point.z
-                })
-            hands_landmarks.append(kpoints)
-    
-    hands.close()
-    return frame, hands
+        if len(results.multi_hand_landmarks) == 2:
+            hand_types = ["right", "left"]
+        else:
+            hand_types = ["right"]
+            
+
+        for landmarks, hand_type in zip(results.multi_hand_landmarks, hand_types): # For each hand
+            
+
+            assert len(landmarks.landmark) == 21, "Not all landmarks detected"
+            
+            for point, n in zip(landmarks.landmark, [i for i in range(21)]):
+                if len(hand_types) == 1:
+                    
+                    data[f"left_hand_{n}_x"].append("nan")
+                    data[f"left_hand_{n}_y"].append("nan")
+                    data[f"left_hand_{n}_z"].append("nan")
+
+                    data[f"{hand_type}_hand_{n}_x"].append(point.x)
+                    data[f"{hand_type}_hand_{n}_y"].append(point.y)
+                    data[f"{hand_type}_hand_{n}_z"].append(point.z)
+                        
+                else:
+                    data[f"{hand_type}_hand_{n}_x"].append(point.x)
+                    data[f"{hand_type}_hand_{n}_y"].append(point.y)
+                    data[f"{hand_type}_hand_{n}_z"].append(point.z)
+
+                    
+             
+        
+    return data
 
 def main():
 
-    cols = [f"{hand}_{i}_{dim}" for hand in ["right_hand", "left_hand"] for i in range(22) for dim in ['x', 'y', 'z']]
-    dic = {col: [] for col in cols}
+    cols = [f"{hand}_{i}_{dim}" for hand in ["right_hand", "left_hand"] for i in range(21) for dim in ['x', 'y', 'z']]
+    data = {col: [] for col in cols}
+    
     # Read a video
     video_path = 'data/videos/69532.mp4'
     cap = cv2.VideoCapture(video_path)
@@ -81,14 +94,17 @@ def main():
                 print("End of video stream")
                 break
         
-            frame, hand_landamarks = detect_sign_language(frame)
+            data = detect_sign_language(frame, data)
+            # print(data)
             
-            # cv2.imshow('Sign Language Detection', frame)
-            # if cv2.waitKey(1) & 0xFF == ord('q'):
-            #     break
+            
                 
     finally:
-        # Clean up
+        
+        df = pd.DataFrame(data)
+
+        df.head(5)
+        df.to_csv("data/69532.csv", index=False)
         cap.release()
         cv2.destroyAllWindows()
 
