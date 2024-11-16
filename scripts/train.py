256d39a53384f327dee8fba38a00d8a63e7a8f31
niconunezz <niconuwii@gmail.com>
2024-11-16 12:28:13 +0100

    tidying up

--- scripts/train.py
+++ scripts/train.py
@@ -3,59 +3,15 @@ import math
 import torch
 import json
 import torch.nn as nn
-from tqdm import tqdm
 from model.model import Net
 import torch.optim as optim
-from data import CustomDataset 
+from data import CustomDataset
 import torch.nn.functional as F
-from dataclasses import dataclass
 from torch.utils.data import DataLoader
-from transformers.models.speech_to_text import Speech2TextConfig
-import augmentations as A
+from config import cfg
 import tiktoken
 
-@dataclass
-class config:
-    n_dim: int = 208
-    n_heads: int = 4
-    block_size: int = 384
-    max_seq_len: int = 31
-    encoder_layers: int = 8
-    vocab_size: int = 100277
-    n_layer: int = 8
-    dropout: float = 0.1
-    device: str = "cuda" if torch.cuda.is_available() else "cpu"
-    val_files: int = 16
-    epochs: int = 10
-    max_ex: int = None
-    aug = A.Compose([A.Resample(sample_rate = (0.3, 2.), p = 0.8),
-                     #A.TemporalCrop(length = 384, p = 0.5),
-                     A.TimeShift(p = 0.5),
-                     A.SpatialAffine(scale=(0.7,1.3),shear=(-0.2,0.2),shift=(-0.15,0.15),degree=(-30,30),p=0.75)])
 
-    config = Speech2TextConfig.from_pretrained("facebook/s2t-small-librispeech-asr")
-    config.encoder_layers = 0
-    config.decoder_layers = 4
-    config.d_model = n_dim
-    config.max_target_positions = 100277 #?
-    config.num_hidden_layers = 1
-    config.vocab_size = 100277
-    config.bos_token_id = 100258
-    config.eos_token_id = 100257
-    config.decoder_start_token_id = 100258
-    config.pad_token_id = 100259
-    config.num_conv_layers = 0
-    config.conv_kernel_sizes = []
-    config.max_length = n_dim
-    config.input_feat_per_channel = n_dim
-    config.num_beams = 1
-    config.attention_dropout = 0.2
-    # config.dropout = 0.2
-    config.decoder_ffn_dim = 512
-    config.init_std = 0.02
-
-
-cfg = config()
 print(f"Device: {cfg.device}")
 
 torch.set_float32_matmul_precision('high')
@@ -66,7 +22,7 @@ print("Data loaded")
 print(f"Data has {len(data)} examples")
 
 dataloader = DataLoader(data, batch_size=64, shuffle=False)
-model = Net(cfg, cfg.config)
+model = Net(cfg, cfg.decoder_cf)
 model.to(cfg.device)
 
 print(f"Model has {(sum(p.numel() for p in model.parameters() if p.requires_grad)/1e6):.2f} M parameters")
@@ -149,7 +105,7 @@ print(f"Data loader examples: {len(dataloader)}")
 
 verbose = False
 t0 = 0
-for epoch in range(config.epochs):
+for epoch in range(cfg.epochs):
     model.train()
     lr = get_lr(epoch)
     for param_group in optimizer.param_groups:
@@ -159,6 +115,7 @@ for epoch in range(config.epochs):
         
 
         x, mask, y = batch['data'], batch['mask'], batch['target']
+        y = y.type(torch.LongTensor)
         x, y = x.to(cfg.device), y.to(cfg.device)
         mask = mask.to(cfg.device)
 
@@ -166,7 +123,8 @@ for epoch in range(config.epochs):
         logits = model(x, mask, y, verbose = False)
             
         B, T, C = logits.shape
-        targets = y.view(B*T)    
+        targets = y.view(B*T)
+
         loss = F.cross_entropy(logits.view(B*T, C), targets, ignore_index=100259)
 
         t2 = time.time()
