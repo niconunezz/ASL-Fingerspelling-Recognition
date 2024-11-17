fbac81a053f93660db28db31af6886d9828d1166
niconunezz <niconuwii@gmail.com>
2024-11-17 21:10:32 +0100

    back to the old house(tokenizer)

--- scripts/train.py
+++ scripts/train.py
@@ -21,7 +21,7 @@ data = CustomDataset(cfg, aug = cfg.aug, verbose = False)
 print("Data loaded")
 print(f"Data has {len(data)} examples")
 
-dataloader = DataLoader(data, batch_size=64, shuffle=False)
+dataloader = DataLoader(data, batch_size=128, shuffle=True)
 model = Net(cfg, cfg.decoder_cf)
 model.to(cfg.device)
 
@@ -29,7 +29,7 @@ print(f"Model has {(sum(p.numel() for p in model.parameters() if p.requires_grad
 
 mx_lr = 4.5e-3
 mn_lr = 1e-5
-warmup_epoch = 1
+warmup_epoch = 3
 max_epochs = cfg.epochs
 def get_lr(it):
     if it < warmup_epoch:
@@ -44,19 +44,20 @@ def validate(model, config):
     data = CustomDataset(cfg = config, mode="val")
     dataloader = DataLoader(data, batch_size= config.val_files, shuffle=False)
     
-    # vocab = json.load(open("data/supplemental_landmarks/prediction_index_to_character.json"))
-    # vocab['59'] = '<sos>'
-    # vocab['60'] = '<eos>'
-    # vocab['61'] = '<pad>'
+    vocab = json.load(open("data/supplemental_landmarks/prediction_index_to_character.json"))
+    vocab['59'] = '<sos>'
+    vocab['60'] = '<eos>'
+    vocab['61'] = '<pad>'
 
-    enc = tiktoken.get_encoding("cl100k_base")
+    # enc = tiktoken.get_encoding("cl100k_base")
     
 
     model.eval()
     losses = []
     for i,batch in enumerate(dataloader):
         x, mask, y = batch['data'], batch['mask'], batch['target']
-        
+        x = x.type(torch.FloatTensor)
+        y = y.type(torch.LongTensor)
         x, y = x.to(cfg.device), y.to(cfg.device)
         mask = mask.to(cfg.device)
         
@@ -73,17 +74,17 @@ def validate(model, config):
             probs = F.softmax(logits, dim = -1)
             
             for i in range(1):
-                # print(f"Sentence: {''.join([vocab[str(token.item())] for token in y[i]])}")
+                print(f"Sentence: {''.join([vocab[str(token.item())] for token in y[i]])}")
 
-                # print("Guesed: ","".join([vocab[str(i.item())] for i in torch.multinomial(probs[0], 1)]))
-                sent = enc.decode(y[i].tolist())
-                sent =sent.replace("<|fim_middle|>", "")
+                print("Guesed: ","".join([vocab[str(i.item())] for i in torch.multinomial(probs[0], 1)]))
+                # sent = enc.decode(y[i].tolist())
+                # sent =sent.replace("<|fim_middle|>", "")
 
-                print(f"Sentence: {sent}")
+                # print(f"Sentence: {sent}")
 
-                guess = enc.decode([i.item() for i in torch.multinomial(probs[0], 1)])
-                guess = guess.replace("<|fim_middle|>", " <pad> ")
-                print(f"Guesed: {guess}" )
+                # guess = enc.decode([i.item() for i in torch.multinomial(probs[0], 1)])
+                # guess = guess.replace("<|fim_mid dle|>", " <pad> ")
+                # print(f"Guesed: {guess}" )
                 
                 
             
@@ -112,10 +113,11 @@ for epoch in range(cfg.epochs):
         param_group['lr'] = lr
     for i, batch in (enumerate(dataloader)):
         t0 = time.time()
-        
+         
 
         x, mask, y = batch['data'], batch['mask'], batch['target']
         y = y.type(torch.LongTensor)
+        x = x.type(torch.FloatTensor)
         x, y = x.to(cfg.device), y.to(cfg.device)
         mask = mask.to(cfg.device)
 
@@ -125,7 +127,7 @@ for epoch in range(cfg.epochs):
         B, T, C = logits.shape
         targets = y.view(B*T)
 
-        loss = F.cross_entropy(logits.view(B*T, C), targets, ignore_index=100259)
+        loss = F.cross_entropy(logits.view(B*T, C), targets, ignore_index=cfg.decoder_cf.pad_token_id)
 
         t2 = time.time()
         optimizer.zero_grad()
@@ -135,7 +137,7 @@ for epoch in range(cfg.epochs):
         if verbose:
             print(f"Backward took {(t3-t2)*1000:.2f} ms")
 
-        norm = nn.utils.clip_grad_norm_(model.parameters(), 4.0)
+        norm = nn.utils.clip_grad_norm_(model.parameters(), 1.0)
 
         optimizer.step()
         
